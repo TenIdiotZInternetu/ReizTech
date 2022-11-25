da89ae562a299f22318b499e198084835ca3049d
TIZI <90770318+TenIdiotZInternetu@users.noreply.github.com>
2022-11-25 16:07:05 +0100

    Update Program.cs

--- Task2 Branches/Task2  Branches/Program.cs
+++ Task2 Branches/Task2  Branches/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 
 using Task2__Branches;
 
@@ -7,11 +7,12 @@ Branch structure = new Branch(3);
 
 structure.AppendBranch(new Branch(3), 0)
     .AppendBranch(new Branch(4), 1)
-    .AppendBranch(new Branch(1), 1);
+    .AppendBranch(new Branch(2), 1);
     
 structure.GetSubbranch(0)
     .GetSubbranch(1)
     .AppendBranch(new Branch(3), 0);
 
 // Calculating its depth
-Console.WriteLine(Branch.GetDepth(structure));
+Console.WriteLine(Branch.GetDepth(structure, true));
+
